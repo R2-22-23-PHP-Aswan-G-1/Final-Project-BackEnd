08728de92503c02cfe0b691b1fcb3431f8da9d6a
DevMohamedEmad <mohamedemad0592@gmail.com>
2023-02-22 20:37:56 +0200

    track table

---
+++ database/migrations/2023_02_22_183308_create_sub_track_super_track_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_track_super_track', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('track_id');
+            $table->unsignedBigInteger('subtrack_id');
+            $table->foreign('track_id')->references('id')->on('tracks');
+            $table->foreign('subtrack_id')->references('id')->on('sub_track');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_track_super_track');
+    }
+};
