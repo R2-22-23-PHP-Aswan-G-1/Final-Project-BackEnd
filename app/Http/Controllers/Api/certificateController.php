e2f13311bd74872c7a753d81e49de88007b1228f
= <=>
2023-03-13 15:03:52 +0200

    zainab

--- app/Http/Controllers/Api/certificateController.php
+++ app/Http/Controllers/Api/certificateController.php
@@ -13,8 +13,10 @@ class certificateController extends Controller
 {
     public function store(StoreCertificateRequest $request)
     {
+        // dd($request->certificate);
+        $request->certificate->storeAs("public/imgs",$request->certificate->getClientOriginalName());
         Certificate::create([
-            'certificate' => $request->certificate,
+            'certificate' => $request->certificate->getClientOriginalName(),
             'instructor_id' => Auth::user()->instructor->id,
         ]);
         return (['message' => 'succes']);
@@ -22,13 +24,17 @@ class certificateController extends Controller
 
     public function destroy(Certificate $certificate)
     {
-        $allCertificates = Auth::user()->instructor->certificates->get();
-        for ($i = 0; $i < count($allCertificates); $i++) {
-            if ($certificate->id ==   $allCertificates[$i]->id) {
-                $certificate->delete();
-                return (['message' => 'success']);
-            }
-        }
-        return (['message' => 'This Id Is Wrong']);
+        // $allCertificates = Auth::user()->instructor->certificates->get();
+        // for ($i = 0; $i < count($allCertificates); $i++) {
+        //     if ($certificate->id ==   $allCertificates[$i]->id) {
+        //         $certificate->delete();
+        //         return (['message' => 'success']);
+        //     }
+        // }
+        
+            $certificate->delete();
+
+       
+        return response()->json(['message' => 'success']);
     }
 }
